1b147ce2c51c649887ca1728f9fb733b9c07e63b
Scagruj <joaojurgac@gmail.com>
2024-04-17 12:33:31 -0300

    write a few tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,19 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL } = require('./crawl')
 
+test('https test', () => {
+    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+test('http test', () => {
+    expect(normalizeURL('http://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+test('https with path / test', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
+
+test('http with path / test', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
 

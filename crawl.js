c7d02b070c637692681e3f51197de63894a0c6ed
Scagruj <joaojurgac@gmail.com>
2024-04-17 13:06:46 -0300

    created the function to normalize the Url

--- crawl.js
+++ crawl.js
@@ -1,7 +1,17 @@
-function normalizeURL() {}
+function normalizeURL(urlString) {
+    const urlObj = new URL(urlString);
+    let urlPathname = urlObj.pathname;
+    if (urlPathname.length > 1 && urlPathname.endsWith('/')) {
+        urlPathname = urlPathname.slice(0, -1);
+        
+    }
+    const normalizedURL = urlObj.hostname + urlPathname
+    console.log(normalizedURL)
+    return normalizedURL
+}
 
 
-
+normalizeURL('http://blog.boot.dev/')
 
 
 
